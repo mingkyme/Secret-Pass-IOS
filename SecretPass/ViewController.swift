40a3076db3c391ebb83b74cc1326451d8433c3a5
Mingky <mingkyme@gmail.com>
2020-06-16 09:21:45 +0900

    Initial Commit

---
+++ SecretPass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecretPass
+//
+//  Created by 김민기 on 2020/06/16.
+//  Copyright © 2020 Mingky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
